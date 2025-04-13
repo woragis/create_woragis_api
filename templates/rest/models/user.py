f1871f5b062fb513581fe2356de1fd007314b192
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-12 23:58:28 -0300

    Create user model

---
+++ templates/rest/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String
+
+from data.database import Base
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
