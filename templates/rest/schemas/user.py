46a8fdf212c82f1f3bf189062131750c10d65232
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-12 23:58:37 -0300

    Create user schemas

---
+++ templates/rest/schemas/user.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class User(BaseModel):
+    id: str
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class RegisterUser(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class LoginUser(BaseModel):
+    email: str
+    password: str
+
+
+class UpdateUser(BaseModel):
+    email: Optional[str] = None
+    password: Optional[str] = None
+
+
+if __name__ == '__main__':
+    user = RegisterUser(name='Jezreel', email='mastering', password='Hello')
+    print(user.model_dump())
+    print(user.model_dump_json())
